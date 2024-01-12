3355fdf7fec5b857c4e837e8dc42c9d81f7a3579
meahb <meahbachert@gmail.com>
2024-01-12 12:41:02 -0500

    complete part of login page

--- app/config/colors.js
+++ app/config/colors.js
@@ -2,5 +2,7 @@ export default {
     primary: "#fc5c65",
     secondary: "#4ecdc4",
     black: "#000",
-    whit: "#fff",
+    white: "#fff",
+    lavendar: "#DCC5F9",
+    baby_blue: "#92C5FC",
 };
--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import { Image, ImageBackground, StyleSheet, View, Text} from 'react-native';
+import {Image, ImageBackground, StyleSheet, View, Text} from 'react-native';
+
+import colors from '../config/colors';
 
 function WelcomeScreen(props) {
     return (
-        <ImageBackground
-          style={styles.background}
-          source={require("../assets/background.jpg")}
-        >
-            <View style={styles.logoContainer}>
-            <Image style={styles.logo} source={require('../assets/logo-red.png')}/>
-            <Text>Sell What You Don't Need</Text>
-            </View>
-            <View style={styles.loginButton}></View>
-            <View style={styles.registerButton}></View>
-        </ImageBackground>
+        <View style={styles.container}>
+            <ImageBackground
+                style={styles.background}>
+                <Image
+                    source={require('../assets/login.png')}
+                    style={styles.gradient}
+                />
+            </ImageBackground>
+        </View>
     );
 }
 
@@ -21,27 +21,13 @@ function WelcomeScreen(props) {
 const styles = StyleSheet.create({
     background: {
         flex: 1,
-        justifyContent: "flex-end",
-        alignItems: 'center',
+        backgroundColor: colors.white,
     },
-    loginButton: {
-        width: "100%",
-        height: 70,
-        backgroundColor: "#fc5c65",
+    container: {
+        flex: 1,
     },
-    logo: {
-        width: 100,
-        height: 100,
-    },
-    logoContainer: {
-        position: 'absolute',
-        top: 70,
-        alignItems: 'center',
-    },
-    registerButton: {
-        width: "100%",
-        height: 70,
-        backgroundColor: "#4ecdc4",
+    gradient: {
+        flex: 0.8,
     },
 });
 
