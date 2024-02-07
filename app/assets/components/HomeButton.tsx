c607bd1a737ee59dd420986727fa0bad1910d8a8
meahb <meahbachert@gmail.com>
2024-02-06 21:47:27 -0500

    feat: add home button component

---
+++ app/assets/components/HomeButton.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { TouchableOpacity, StyleSheet } from 'react-native';
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+import { useNavigation } from '@react-navigation/native';
+
+import { colors } from '../../config/theme';
+
+export default function HomeButton() {
+  const navigation = useNavigation();
+  const onPress = () => {navigation.navigate('Home' as never);
+  };
+
+  return (
+    <TouchableOpacity 
+      style={styles.container}
+      onPress={onPress}
+    >
+      <FontAwesome  
+        name='home'
+        size={55} 
+        color={colors.white}
+      />
+    </TouchableOpacity>
+  );
+}
+  
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.black,
+    height: 80,
+    width: 80,
+    borderRadius: 50,
+    marginTop: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    bottom: 40,
+    shadowColor: colors.black,
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.5,
+    shadowRadius: 5,
+    elevation: 5,
+  },
+});
