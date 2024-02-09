3fd6a5532740657886fa823e6655955ae3d82bbe
meahb <meahbachert@gmail.com>
2024-02-08 21:28:37 -0500

    feat: handle button press for event buttons

--- app/assets/components/EventButtons.tsx
+++ app/assets/components/EventButtons.tsx
@@ -1,57 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
-//import { useNavigation } from '@react-navigation/native';
 
 import Button from './Buttons';
 
-export default function EventButtons ({ 
-    variant,
-}) {
-    //const navigation = useNavigation();
+export default function EventButtons ({variant}) {
+    const [selectedVariant, setSelectedVariant] = useState(variant === 'four' || variant === 'build' ? 'Vault' : 'Bars');
+    const handleButtonPress = (selectedVariant: string) => {
+    setSelectedVariant(selectedVariant);
+    };
+
     return (
-    <View style={styles.container}>
-            {variant === 'four' && (
+        <View style={styles.container}>
+            {(variant === 'four' || variant === 'build') && (
                 <Button
-                        onPress={console.log('Vault')}
-                        variant='black'
-                        title='Vault'
-                        style={styles.button}/>
+                    onPress={() => handleButtonPress('Vault')}
+                    variant={selectedVariant === 'Vault' ? 'black' : 'white'}
+                    title='Vault'
+                    style={variant === 'build' ? styles.buildButton : styles.button}/>
             )}
-                <Button
-                        onPress={console.log('Bars')}
-                        variant='white'
-                        title='Bars'
-                        style={styles.button}
-                />
-                <Button
-                        onPress={console.log('Beam')}
-                        variant='white'
-                        title='Beam'
-                        style={styles.button}
-                />
-                <Button
-                        onPress={console.log('Floor')}
-                        variant='white'
-                        title='Floor'
-                        style={styles.button}
-                />
-    </View>
+            <Button
+                onPress={() => handleButtonPress('Bars')}
+                variant={selectedVariant === 'Bars' ? 'black' : 'white'}
+                title='Bars'
+                style={variant === 'build' ? styles.buildButton : styles.button}/>
+            <Button
+                onPress={() => handleButtonPress('Beam')}
+                variant={selectedVariant === 'Beam' ? 'black' : 'white'}
+                title='Beam'
+                style={variant === 'build' ? styles.buildButton : styles.button}/>
+            <Button
+                onPress={() => handleButtonPress('Floor')}
+                variant={selectedVariant === 'Floor' ? 'black' : 'white'}
+                title='Floor'
+                style={variant === 'build' ? styles.buildButton : styles.button}/>
+        </View>
     );
 }
 
-    const styles = StyleSheet.create({
-        container: {
-            flexDirection: 'row',
-            justifyContent: 'center',
-            alignItems: 'center',
-            position: 'absolute',
-            top: 180,
-            marginVertical: 10,
-
-        },
-        button: {
-            width: 80,
-            height: 50,
-            marginHorizontal: 5,
-        },
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'absolute',
+        top: 180,
+        marginVertical: 10,
+    },
+    button: {
+        width: 80,
+        height: 50,
+        marginHorizontal: 5,
+    },
+    buildButton: {
+        width: 70,
+        height: 45,
+        marginHorizontal: 5,
+    },
 });
