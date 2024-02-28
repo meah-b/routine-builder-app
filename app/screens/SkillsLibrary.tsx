0827bd2e8c7b1a87918d067b2e716871aa445fd5
meahb <meahbachert@gmail.com>
2024-02-27 21:54:47 -0500

    feat: implement conditional rendering of skill forms

--- app/screens/SkillsLibrary.tsx
+++ app/screens/SkillsLibrary.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, View, TouchableOpacity } from 'react-native';
 import {LinearGradient} from 'expo-linear-gradient';
 
 import { CustomText, colors } from '../config/theme';
@@ -7,16 +7,48 @@ import Header from '../assets/components/Header';
 import HomeButton from '../assets/components/buttons/HomeButton';
 import EventButtons from '../assets/components/buttons/EventButtons';
 import Button from '../assets/components/buttons/Buttons';
+import SkillForm from '../assets/components/SkillForm';
+
 
 export default function SkillLibrary() {
+    const [form, setForm] = useState(0);
+
+    function Form(){
+        if (form === 0){
+            return (
+                <View style={styles.container}>
+                    <CustomText style={styles.h1} bold>Skill Library</CustomText>
+                    <EventButtons variant='four'></EventButtons>
+                    <Button
+                        style={styles.addButton}
+                        title='Add Skill'
+                        variant='black'
+                        onPress={() => setForm(1)}
+                    ></Button>
+                </View>
+            )
+        } else {
+            return (
+                <View>
+                    <TouchableOpacity onPress={() => setForm(0)} style={{marginBottom: 15, marginTop: 60, left: 5}}>
+                        <CustomText 
+                            style={styles.text} 
+                            bold
+                        >back to skills library
+                        </CustomText>
+                    </TouchableOpacity>
+                    <SkillForm event="Beam" /> 
+                </View>
+            )
+        }
+    }
+
     return (
         <LinearGradient
             colors={colors.gradient}
             style={styles.container}>
             <Header></Header>
-            <CustomText style={styles.text} bold>Skill Library</CustomText>
-            <EventButtons variant='four'></EventButtons>
-            <Button style={styles.addButton} title='Add Skill' variant='black' onPress={() => console.log('Add Skill')}></Button>
+            <Form/>
             <HomeButton></HomeButton>
         </LinearGradient>
     );
@@ -28,7 +60,7 @@ const styles = StyleSheet.create({
         height: 35,
         width: 110,
         top: 260,
-        left: 20,
+        right: 65,
     },
     container: {
         flex: 1,
@@ -36,10 +68,16 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
     },
-    text: {
+    h1: {
         color: colors.black,
         fontSize: 30,
         position: 'absolute',
         top: 130,
     },
+    text: {
+        fontSize: 18, 
+        textDecorationLine:'underline', 
+        color: colors.black,
+    }
+    
 });
