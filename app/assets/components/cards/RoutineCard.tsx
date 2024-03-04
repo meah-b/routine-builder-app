53874af2958b9756a476beb1cdf219e2c0c283b5
meahb <meahbachert@gmail.com>
2024-03-04 18:57:09 -0500

    feat: make routine cards expandable

--- app/assets/components/cards/RoutineCard.tsx
+++ app/assets/components/cards/RoutineCard.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import Button from '../buttons/Buttons';
+import { colors, CustomText } from '../../../config/theme';
+import {AntDesign} from '@expo/vector-icons';
 
-import {colors, CustomText} from '../../../config/theme';
+export default function RoutineCard({ name, sv, handleDelete, skills, connections}) {
+    const [expanded, setExpanded] = useState(false);
 
+    const handleExpand = () => {
+        setExpanded(!expanded);
+    };
 
-export default function RoutineCard({
-    name,
-    sv,
-}) {
     return (
-        <View style={styles.container}>
+        <View style={expanded ? styles.expandedContainer : styles.container}>
             <View style={styles.col}>
-                <CustomText style={styles.name} bold>{name}</CustomText>
-                <CustomText style={styles.difficulty}>Start Value: {sv}</CustomText>
+                <CustomText style={styles.name} bold>
+                    {name}
+                </CustomText>
+                <CustomText style={styles.text}>Start Value: {sv}</CustomText>
+                {expanded && (
+                    <>
+                        <CustomText style={styles.text}>Skills: {skills}</CustomText>
+                        <CustomText style={styles.text}>Connections: {connections}</CustomText>
+                    </>
+                )}
             </View>
             <Button
-            onPress={console.log('view more button pressed')}
-            variant='black'
-            title='View More'
-            style={{width: 120, height: 45}}
+                onPress={handleExpand}
+                variant="black"
+                title={expanded ? 'Hide Details' : 'View More'}
+                style={{ width: expanded ? 140 : 120, height: 45 }}
             />
+            {expanded && <AntDesign 
+                    color="black" 
+                    name="delete" 
+                    size={24}  
+                    style={styles.icon}
+                    onPress={handleDelete}
+                />}
         </View>
     );
 }
@@ -28,7 +45,7 @@ export default function RoutineCard({
 const styles = StyleSheet.create({
     col: {
         flexDirection: 'column',
-        gap: 4
+        gap: 3,
     },
     container: {
         flexDirection: 'row',
@@ -42,15 +59,38 @@ const styles = StyleSheet.create({
         margin: 4,
         borderRadius: 15,
         shadowColor: colors.black,
-        shadowOffset: {width: 0, height: 4},
+        shadowOffset: { width: 0, height: 4 },
         shadowOpacity: 0.5,
         shadowRadius: 5,
         elevation: 5,
     },
-    difficulty: {
-        fontSize: 20
+    text: {
+        fontSize: 20,
+    },
+    expandedContainer: {
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        backgroundColor: colors.white,
+        color: colors.black,
+        width: 350,
+        height: 'auto',
+        margin: 4,
+        paddingVertical: 5,
+        borderRadius: 15,
+        shadowColor: colors.black,
+        shadowOffset: { width: 0, height: 4 },
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        elevation: 5,
+    },
+    icon: {
+        position: 'absolute',
+        top: 15,
+        right: 20
     },
     name: {
-        fontSize: 20
-    }
+        fontSize: 20,
+        right: 5,
+    },
 });
