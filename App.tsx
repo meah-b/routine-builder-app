aa52327a7cd8337900f9d98731f9149e4dadf1df
Meah Bachert <143766025+meahb@users.noreply.github.com>
2024-04-01 20:56:59 -0400

    feat: create different stack for coaches portal

--- App.tsx
+++ App.tsx
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { User, onAuthStateChanged } from 'firebase/auth';
-import { firebase_auth } from './app/Firebase/firebaseConfig';
+import { firebase_auth, firestore_db } from './app/Firebase/firebaseConfig';
+import { getDoc, doc } from 'firebase/firestore';
 
 import LoginScreen from './app/screens/LoginScreen';
 import HomeScreen from './app/screens/HomeScreen';
@@ -13,12 +14,13 @@ import RoutineBuilder from './app/screens/RoutineBuilder';
 import Profile from './app/screens/Profile';
 import NotifScreen from './app/screens/NotificationScreen';
 import SignUpScreen from './app/screens/SignUpScreen';
+import RosterScreen from './app/screens/RosterScreen';
 
 
 const Stack = createNativeStackNavigator();
 const InsideStack = createNativeStackNavigator();
 
-function InsideLayout() {
+function AthleteLayout() {
   return (
     <InsideStack.Navigator screenOptions={{ headerShown: false }}>
       <InsideStack.Screen name='Home' component={HomeScreen} />
@@ -32,20 +34,47 @@ function InsideLayout() {
   )
 }
 
+function CoachLayout() {
+  return (
+    <InsideStack.Navigator screenOptions={{ headerShown: false }}>
+      <InsideStack.Screen name='Roster' component={RosterScreen} />
+      <InsideStack.Screen name='Home' component={HomeScreen} />
+      <InsideStack.Screen name='Skill Library' component={SkillLibrary} />
+      <InsideStack.Screen name='Connection Library' component={ConnectionLibrary} />
+      <InsideStack.Screen name='Routine Library' component={RoutineLibrary} />
+      <InsideStack.Screen name='Routine Builder' component={RoutineBuilder} />
+      <InsideStack.Screen name='Profile' component={Profile} />
+      <InsideStack.Screen name='Notif Screen' component={NotifScreen} />
+    </InsideStack.Navigator>
+  )
+}
+
 export default function App() {
   const [user, setUser] = useState<User | null>(null);
+  const [userType, setUserType] = useState('');
 
   useEffect(() => {
     onAuthStateChanged(firebase_auth, (user) => {
       setUser(user)
+      const user_uid = user.uid;
+      const user_doc = doc(firestore_db, 'users', user_uid);
+      getDoc(user_doc)
+          .then((docSnapshot) => {
+            if (docSnapshot.exists()) {
+              setUserType(docSnapshot.data().account_type);
+            }
+          })
     })
   })
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Login' screenOptions={{ headerShown: false }}>
-        {user ? (
-          <Stack.Screen name='Inside' component={InsideLayout} />
+        {user ? (userType === 'Athlete' ? (
+          <Stack.Screen name='Athlete' component={AthleteLayout} />
         ) : (
+          <Stack.Screen name='Coach' component={CoachLayout} />
+        )) : (
           <>
             <Stack.Screen name='Login' component={LoginScreen} />
             <Stack.Screen name='Sign Up' component={SignUpScreen} />
